6494e95ca2a52e36eb9bdc91d0aa24111338288f
mobile-bungalow <pwmay.work@gmail.com>
2024-11-25 12:33:55 -0800

    remove all extensions

--- swiftui/LiveViewNativeGo/Screens/AppsScreen.swift
+++ swiftui/LiveViewNativeGo/Screens/AppsScreen.swift
@@ -5,26 +5,21 @@
 //  Created by Carson Katri on 3/26/24.
 //
 
-import SwiftUI
 import Foundation
-import VisionKit
 import LiveViewNative
-import LiveViewNativeLiveForm
-import LiveViewNativeAVKit
-import LiveViewNativeCharts
-import LiveViewNativeMapKit
+import SwiftUI
 import TipKit
+import VisionKit
 
 /// The current app to display, with a unique ID for each instance of this app launched.
 struct SelectedApp: Identifiable, Hashable, Codable {
     let url: URL
     let id: UUID
-    
+
     @ViewBuilder
     func makeLiveView(settings: Settings, dynamicType: DynamicTypeSize) -> some View {
         #LiveView(
-            url,
-            addons: [.liveForm, .avKit, .charts, .mapKit]
+            url
         ) {
             ConnectingView(url: url)
         } disconnected: {
@@ -60,71 +55,76 @@ struct QuickActionsTip: Tip {
 /// The app scanning and rendering screen.
 struct AppsScreen: View {
     @State private var selection: SelectedApp?
-    
+
     @State private var showCodeScanner: Bool = false
-    
+
     @AppStorage("customURL") private var inputURL: String = ""
-    
+
     @Environment(Settings.self) private var settings
-    
+
     @Environment(\.dynamicTypeSize) private var dynamicType
-    
+
     #if os(macOS)
-    @Environment(\.openWindow) private var openWindow
+        @Environment(\.openWindow) private var openWindow
     #endif
-    
+
     var body: some View {
         #if os(iOS)
-        List {
-            Section {
-                if settings.recentURLs.isEmpty {
-                    Text("No recent apps connected.")
-                }
-                ForEach(settings.recentURLs.reversed(), id: \.self) { url in
-                    Button {
-                        selection = .init(url: url, id: UUID())
-                        settings.recentURLs += [url]
-                    } label: {
-                        Label {
-                            Text((url as NSURL).resourceSpecifier.flatMap({ String($0.dropFirst(2)) }) ?? url.absoluteString)
-                        } icon: {
-                            AsyncImage(url: url.replacing(path: "/apple-touch-icon.png")) { phase in
-                                switch phase {
-                                case .success(let image):
-                                    image
-                                        .resizable()
-                                case .failure:
-                                    AsyncImage(url: url.replacing(path: "/favicon.ico")) { phase in
-                                        switch phase {
-                                        case .success(let image):
-                                            image
-                                                .resizable()
-                                        default:
-                                            Rectangle()
-                                                .fill(.quaternary)
+            List {
+                Section {
+                    if settings.recentURLs.isEmpty {
+                        Text("No recent apps connected.")
+                    }
+                    ForEach(settings.recentURLs.reversed(), id: \.self) { url in
+                        Button {
+                            selection = .init(url: url, id: UUID())
+                            settings.recentURLs += [url]
+                        } label: {
+                            Label {
+                                Text(
+                                    (url as NSURL).resourceSpecifier.flatMap({
+                                        String($0.dropFirst(2))
+                                    }) ?? url.absoluteString)
+                            } icon: {
+                                AsyncImage(url: url.replacing(path: "/apple-touch-icon.png")) {
+                                    phase in
+                                    switch phase {
+                                    case .success(let image):
+                                        image
+                                            .resizable()
+                                    case .failure:
+                                        AsyncImage(url: url.replacing(path: "/favicon.ico")) {
+                                            phase in
+                                            switch phase {
+                                            case .success(let image):
+                                                image
+                                                    .resizable()
+                                            default:
+                                                Rectangle()
+                                                    .fill(.quaternary)
+                                            }
                                         }
+                                    default:
+                                        Rectangle()
+                                            .fill(.quaternary)
                                     }
-                                default:
-                                    Rectangle()
-                                        .fill(.quaternary)
                                 }
+                                .frame(width: 30, height: 30)
+                                .clipShape(.rect(cornerRadius: 8, style: .continuous))
                             }
-                            .frame(width: 30, height: 30)
-                            .clipShape(.rect(cornerRadius: 8, style: .continuous))
                         }
                     }
-                }
-            } header: {
-                HStack {
-                    Text("Recents")
-                    Spacer()
-                    Button("Clear") {
-                        settings.recentURLs = []
+                } header: {
+                    HStack {
+                        Text("Recents")
+                        Spacer()
+                        Button("Clear") {
+                            settings.recentURLs = []
+                        }
+                        .controlSize(.mini)
                     }
-                    .controlSize(.mini)
                 }
             }
-        }
             .safeAreaInset(edge: .bottom) {
                 VStack {
                     Divider()
@@ -146,7 +146,7 @@ struct AppsScreen: View {
                                 }
                             }
                         }
-                        
+
                         HStack {
                             VStack { Divider() }
                             Text("OR")
@@ -155,7 +155,7 @@ struct AppsScreen: View {
                             VStack { Divider() }
                         }
                         .padding(.vertical)
-                        
+
                         HStack {
                             TextField("Enter URL", text: $inputURL)
                                 .autocorrectionDisabled()
@@ -188,9 +188,9 @@ struct AppsScreen: View {
             }
             .onContinueUserActivity(NSUserActivityTypeBrowsingWeb) { activity in
                 guard selection == nil,
-                      let webpageURL = activity.webpageURL,
-                      let components = URLComponents(url: webpageURL, resolvingAgainstBaseURL: true),
-                      let liveViewURL = components.queryItems?
+                    let webpageURL = activity.webpageURL,
+                    let components = URLComponents(url: webpageURL, resolvingAgainstBaseURL: true),
+                    let liveViewURL = components.queryItems?
                         .first(where: { $0.name == "liveview" })
                         .flatMap(\.value)
                         .flatMap(URL.init)
@@ -199,41 +199,42 @@ struct AppsScreen: View {
                 settings.recentURLs += [liveViewURL]
             }
         #else
-        VStack {
             VStack {
-                TextField("URL", text: $inputURL)
-                    .autocorrectionDisabled()
-                    .textFieldStyle(.roundedBorder)
+                VStack {
+                    TextField("URL", text: $inputURL)
+                        .autocorrectionDisabled()
+                        .textFieldStyle(.roundedBorder)
+                    Button {
+                        guard let url = URL(string: appendingScheme(to: inputURL))
+                        else { return }
+                        openWindow(value: SelectedApp(url: url, id: .init()))
+                        settings.recentURLs += [url]
+                    } label: {
+                        Label("Launch", systemImage: "arrow.up.right.square.fill")
+                            .frame(maxWidth: .infinity)
+                    }
+                }
+                Divider()
                 Button {
-                    guard let url = URL(string: appendingScheme(to: inputURL))
-                    else { return }
+                    let url = URL(string: "http://localhost:4000")!
                     openWindow(value: SelectedApp(url: url, id: .init()))
                     settings.recentURLs += [url]
                 } label: {
-                    Label("Launch", systemImage: "arrow.up.right.square.fill")
+                    Label("Launch Local Host", systemImage: "network")
                         .frame(maxWidth: .infinity)
                 }
+                .buttonStyle(.bordered)
             }
-            Divider()
-            Button {
-                let url = URL(string: "http://localhost:4000")!
-                openWindow(value: SelectedApp(url: url, id: .init()))
-                settings.recentURLs += [url]
-            } label: {
-                Label("Launch Local Host", systemImage: "network")
-                    .frame(maxWidth: .infinity)
-            }
-            .buttonStyle(.bordered)
-        }
-        .controlSize(.extraLarge)
-        .buttonStyle(.borderedProminent)
-        .padding()
-        .navigationTitle("LiveView Native Go")
+            .controlSize(.extraLarge)
+            .buttonStyle(.borderedProminent)
+            .padding()
+            .navigationTitle("LiveView Native Go")
         #endif
     }
-    
+
     func appendingScheme(to inputURL: String) -> String {
-        inputURL.starts(with: "http://") || inputURL.starts(with: "https://") ? inputURL : "http://\(inputURL)"
+        inputURL.starts(with: "http://") || inputURL.starts(with: "https://")
+            ? inputURL : "http://\(inputURL)"
     }
 }
 
@@ -241,5 +242,5 @@ struct AppsScreen: View {
     NavigationStack {
         AppsScreen()
     }
-        .environment(Settings())
+    .environment(Settings())
 }
